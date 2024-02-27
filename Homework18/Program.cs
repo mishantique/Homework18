05f345225c22366ee81d00f8f7007b4239b991ae
mishantique <michvitko@mail.ru>
2024-02-27 20:44:41 +0300

    Добавьте файлы проекта.

---
+++ Homework18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+            Console.ReadKey();
+
+        }
+
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dict = new Dictionary<char, char>
+            {
+                {'(', ')'},
+                {'{', '}'},
+                {'[', ']'},
+            };
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(dict[c]);
+                }
+
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop()!= c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
